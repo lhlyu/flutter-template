c73e312d87021fbd3d984fe220eca0b8c94791c4
lhlyu <41979509+lhlyu@users.noreply.github.com>
2024-01-12 05:06:17 +0000

    Deploying to gh-pages from @ lhlyu/flutter_template@a3e7feea1ee0c02d31e1cb9e295fc0d4660d9394 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9692e6a2dac4ad01af0fd4a3db0756bd",
-"index.html": "90f7f7303c52a38c75ea20e66fc7dcd8",
-"/": "90f7f7303c52a38c75ea20e66fc7dcd8",
+"index.html": "a48bf01c20493f2a2402f19b0a24f8c4",
+"/": "a48bf01c20493f2a2402f19b0a24f8c4",
 "main.dart.js": "d6b83cd2f43269d04e8bd1b49b7756aa",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
